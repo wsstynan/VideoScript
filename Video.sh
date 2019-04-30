5c4d642e5e02a064a0bc3fe736ee8cf6383bce54
Shane Tynan <shane.tynan@websummit.com>
2019-04-30 15:41:00 +0100

    Add Video Script

---
+++ Video.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+# get rid of the cursor
+setterm -cursor off
+
+# set here the path to the directory containing your videos
+VideoLocation="~/Desktop/Content"
+# you can probably leave this alone
+Process="omxplayer"
+# our loop
+while true; do
+        if ps ax | grep -v grep | grep $Process > /dev/null
+        then
+        sleep 1;
+else
+        for entry in $VideoLocation/*
+        do
+                clear
+                # -r for stretched over the entire location
+                omxplayer -r $entry > /dev/null
+        done
+fi
+done
